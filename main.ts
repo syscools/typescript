728ec8704d87ca953b2645750aac1f326ff62c2d
dexter aparicio <syscools@gmail.com>
2017-04-17 15:47:56 +0800

    use setTimeout() in typescript

--- main.ts
+++ main.ts
@@ -3,9 +3,13 @@ import { AppComponent } from './app.component';
 function main() {
     console.log("** main **");
 
-    let obj = new AppComponent();
-    let s = obj.add(3,7);
-    console.log("sum = " + s);
+    setTimeout(function () {
+        let obj = new AppComponent();
+        let s = obj.add(3,7);
+        console.log("sum = " + s);
+    },5000);
+
+    console.log("** exiting **");
 }
 
 main();
